f83cb61c3b4e61ee86845e70c822299d17f8d450
amiroucha <amirou.septiembre@gmail.com>
2025-02-05 20:06:15 +0100

    METIDAS COSAS DEL FIREBASE

---
+++ Firebaseconfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzyAyEKHI7nAjr4BNUGBBM6TAdqX1hxGg",
+  authDomain: "login-firebase-bf0cc.firebaseapp.com",
+  projectId: "login-firebase-bf0cc",
+  storageBucket: "login-firebase-bf0cc.firebasestorage.app",
+  messagingSenderId: "82396762647",
+  appId: "1:82396762647:web:d8f2aa0787563d7a288252"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
