d13ae96e9270d442aa32cd7b97ca14858a22c40e
amiroucha <amirou.septiembre@gmail.com>
2025-02-05 20:39:10 +0100

    me falta la info del return

---
+++ app/tabs/index.tsx
@@ -0,0 +1,39 @@
+import {auth} from "@/Firebaseconfig";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import { useState } from "react";
+import { Alert } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+
+
+export default function Index() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const logIn = async () => {
+        try{
+            const user = await (signInWithEmailAndPassword(auth,email,password))
+
+
+        }catch (error:any){
+            console.log(error)
+            Alert.alert("Inicio de sesion incorrecto", "error.message")
+        }
+    }
+
+
+    return(
+        <View>
+
+            
+
+        </View>
+
+
+
+
+    )
+
+
+
+}
