c21db60a9cb5b285a6b11393a8920952d030075d
amiroucha <amirou.septiembre@gmail.com>
2025-02-05 20:16:32 +0100

    parece que finalizada la conexion de la base datos

--- Firebaseconfig.ts
+++ Firebaseconfig.ts
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+//lo que añado 
+//auth tengo los usuarios 
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+//db tengo la base de datos
+
+export const db = getFirestore(app);
+
