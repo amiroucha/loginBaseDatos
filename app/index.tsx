5be819ec530ed6a701081ddfe63cce358b6f6f29
amiroucha <amirou.septiembre@gmail.com>
2025-02-10 19:53:50 +0100

    metido lo de la base de datos ??

--- app/index.tsx
+++ app/index.tsx
@@ -2,42 +2,58 @@ import { GlobalStyles } from '@/theme/GlobalStyles';
 import { View, Text, TextInput, Image, Pressable, ScrollView  } from 'react-native';
 import Boton from '../components/Boton';
 import { Alert } from "react-native";
+import { useState } from "react";
+import { router} from "expo-router";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '@/Firebaseconfig';
+
 
 
 const login = () => {
-    return (
-      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      const [email, setEmail] = useState('');
+      const [password, setPassword] = useState('');
+    
+    
+      const logIn = async () => {
+        try {
+          const user = await (signInWithEmailAndPassword(auth,email,password));
+          if (user) router.replace('./tabs')
+        } catch (error:any) {
+          console.log(error);
+          Alert.alert("Inicio de sesión incorrecto", "error.message");
+        }
+      }
+    
+    
+      const register = async () => {
+        try {
+          const user = await (createUserWithEmailAndPassword(auth,email,password));
+          if (user) router.replace('./tabs');
+        } catch (error: any) {
+          console.log(error);
+          Alert.alert("Error al registrar el usuario", error.message);
+        }
+    
+    
+      }
+    
+      return (
         <View style={GlobalStyles.contenedor}>
-
-        
-          <Text style={GlobalStyles.tituloLogin}>LOGIN</Text>
-          <Image 
-            source={require("../assets/images/user.png")}
-            style={GlobalStyles.LoginImg}
-          ></Image>
-          <TextInput style={GlobalStyles.inputLogin} 
-          placeholder="Usuario"></TextInput>
-
-          <TextInput style={GlobalStyles.inputLogin} 
-          placeholder="Contraseña" secureTextEntry={true} ></TextInput>
-
-
-          <View style={{display:'flex',flexDirection: 'row', justifyContent:'space-between', width:'100%', alignItems:'center'}}>
-            <Pressable onPress= {()=>{
-              Alert.alert('En proceso de registro')}}>
-                <Text style={GlobalStyles.enlaceRegistro}>Regístrate aquí</Text>
+          <Text style={GlobalStyles.tituloLogin}> Página de login</Text>
+          <TextInput style={GlobalStyles.inputLogin} placeholder="email"  value={email} onChangeText={setEmail} />
+          <TextInput style={GlobalStyles.inputLogin} placeholder='contraseña' value={password} onChangeText={setPassword}
+            secureTextEntry />
+          <View style={GlobalStyles.contenedor}>
+            <Pressable style={GlobalStyles.boton} onPress={logIn}>
+              <Text style={GlobalStyles.boton}>Login</Text>
             </Pressable>
-
-            <Boton label='Acceder' link='../tabs'></Boton>
+            <Pressable style={GlobalStyles.boton} onPress={register}>
+              <Text style={GlobalStyles.boton}>Registrarse</Text>
+            </Pressable>
           </View>
+    
+    
         </View>
-
-
-
-
-      </ScrollView>
-      
-  
-    )
+      ); 
   }
   export default login
